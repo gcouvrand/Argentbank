d778a2349fd9a13cd25cc7ac2c2ca1d610aec5f7
Gary <vesperocyon@gmail.com>
2022-04-20 13:20:12 +0200

    First commit

--- src/components/SignInBox/index.jsx
+++ src/components/SignInBox/index.jsx
@@ -1,4 +1,49 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { Login, UserInfos } from "../../services/ApiCall"
+
 function SignInBox(props) {
+
+    const [email, setEmail] = useState({"email": ""})
+    const [password, setPassword] = useState({"password": ""})
+    const dispatch = useDispatch();
+
+    const onChangeEmail = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const onChangePassword = (e) => {
+        setPassword(e.target.value);
+    }
+    
+    const handleSubmit = async (e) => {
+        try {
+        e.preventDefault()
+        const token = await Login(email, password)
+            if (token) {
+                dispatch({
+                    type: "SignIn",
+                    token: token,
+                    loggedIn: true,
+                })
+            }
+            UserInfos(token)
+            const infos = await UserInfos(token)
+            dispatch({
+                type: "UpdateName",
+                firstName: infos.firstName,
+                lastName : infos.lastName,
+            })
+            } catch (e) {
+                console.log(e);
+            }
+    }
+ 
+
+    if(useSelector(state => state.Actions.loggedIn))
+        return <Navigate to="/user"/>
+
     return(
         <section className="sign-in-content">
             <i className="fa fa-user-circle sign-in-icon"></i>
@@ -6,17 +51,17 @@ function SignInBox(props) {
             <form>
                 <div className="input-wrapper">
                     <label for="username">Username</label>
-                    <input type="text" id="username" />
+                    <input type="text" id="username" onChange={onChangeEmail} />
                 </div>
                 <div className="input-wrapper">
                     <label for="password">Password</label>
-                    <input type="password" id="password" />
+                    <input type="password" id="password" onChange={onChangePassword} />
                 </div>
                 <div className="input-remember">
                     <input type="checkbox" id="remember-me" />
                     <label for="remember-me">Remember me</label>
                 </div>
-                <button className="sign-in-button">Sign In</button>
+                <button className="sign-in-button" onClick={handleSubmit}>Sign In</button>
             </form>
         </section>
     )
